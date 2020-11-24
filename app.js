be0d8165dce72e063018a4d0a2b783beb2c09a13
Titi <christianpham.vidpb@gmail.com>
2020-11-24 19:57:29 +0100

    fst commt

---
+++ app.js
@@ -0,0 +1,13 @@
+const express= require("express");
+const app= express();
+const env= require("./config/env");
+const port= env;
+console.log(process.env.PORT);
+app.get("/", (req,res) => {
+    res.sendFile(__dirname + "/hello.html");
+});
+
+
+
+
+app.listen(port, () => console.log("server is running"));
