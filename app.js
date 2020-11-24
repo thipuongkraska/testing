b20a110a417bacc8fd26b52867d7232b4345bd8a
Titi <christianpham.vidpb@gmail.com>
2020-11-24 20:10:53 +0100

    commit second

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express= require("express");
 const app= express();
 const env= require("./config/env");
 const port= env;
+console.log("it is working");
 console.log(process.env.PORT);
 app.get("/", (req,res) => {
     res.sendFile(__dirname + "/hello.html");
